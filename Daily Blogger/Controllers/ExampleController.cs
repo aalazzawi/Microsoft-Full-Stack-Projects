14506a2b9749e4453b558ff139702dfe7d27c496
aalazzawi <66639502+aalazzawi@users.noreply.github.com>
2021-08-22 14:03:12 -0400

    Add files via upload

---
+++ Daily Blogger/Controllers/ExampleController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DailyBlogger.Controllers
+{
+    // this controler has 5 method perform the following
+    // creat views to show the needed information 
+    // desplay an error message if information is not found
+    // desplay a certin file as a response
+    // desplay a json file with the needed information as a response
+    // redirect the user to new page 
+    public class ExampleController : Controller
+    {
+        public IActionResult ShowMeAPage()
+        {
+            return View();
+        }
+
+        public IActionResult ShowMeAnError()
+        {
+            return NotFound();
+        }
+
+        public IActionResult GiveMeAFile()
+        {
+            return File("/mydoc.txt", "text/plain");
+        }
+
+        public IActionResult GiveMeSomeJson()
+        {
+            string jsondata = "{Name: John-Luc Picard, Rank: Captian, Starship: Enterprise}";
+
+            return Json(jsondata);
+        }
+
+
+        public IActionResult TakeMeToGoogle()
+        {
+            return Redirect("https://google.com");
+        }
+    }
+}
---
+++ Daily Blogger/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DailyBloger.Models;
+
+namespace DailyBloger.Controllers
+{
+    // this controler has 5 method perform the following
+    // return the home content for the user
+    // return the description page of the app with a message
+    // return the contact page of the app with a message
+    // return the privacy page
+    /* reduces the amount of work the web server performs to generate a response by setting 
+       the parameters of the cash control using the resources class and interfaces */
+    // finaly return an appropriate View template instade of a message 
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
